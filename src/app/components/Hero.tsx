b1638ac1d2a3067a2bb84db82ddb69a680076ff3
alaina <alainasid123@gmail.com>
2025-09-08 02:31:57 +0530

    catgry listing done

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -2,46 +2,45 @@ import Image from "next/image";
 import Navbar from "./Navbar";
 import SearchBox from "./SearchBox";
 import CategoryTabs from "./CategoryTabs";
+import CarCarousel from "./CarCard";
 
 export default function Hero() {
   return (
     <div>
-    <div className="relative w-[1440px] h-[600px] mx-auto">
-      <Image
-        src="/assets/back.jpg"
-        alt="Hero background"
-        fill
-        className="object-cover"
-        priority
-      />
+      {/* Hero Section */}
+      <div className="relative w-[1440px] h-[600px] mx-auto">
+        <Image
+          src="/assets/back.jpg"
+          alt="Hero background"
+          fill
+          className="object-cover"
+          priority
+        />
 
-      <Navbar />
-  <div className="absolute top-[243px] left-1/2 -translate-x-1/2 w-[1312px] h-[29px] flex items-center justify-center opacity-100 text-white">
-        <h1 className="text-[25px] font-semibold text-center">
-          FIND YOUR DREAM CAR
-        </h1>
+        <Navbar />
+
+        {/* Hero Title */}
+        <div className="absolute top-[243px] left-1/2 -translate-x-1/2 w-[1312px] h-[29px] flex items-center justify-center text-white">
+          <h1 className="text-[25px] font-semibold text-center">
+            FIND YOUR DREAM CAR
+          </h1>
+        </div>
+
+        <SearchBox />
       </div>
-      <SearchBox/>
-      
-    <p
-        className="absolute text-[25px] font-medium text-black "
-        style={{
-          
-    
-          top: "737px",
-          left: "64px",
-          opacity: 1,
-          gap: "24px",
-        
-          padding:"14px"
-        }}
-      >
-        SEARCH BY CATEGORY
-      </p>
-       <div className="absolute top-[800px] h-[80px] left-1/2 -translate-x-1/2 flex">
+
+      {/* Search by Category */}
+      <div className="w-[1312px] mx-auto mt-40">
+        <p className="text-[25px] font-medium text-black mb-6 mx-3">
+          SEARCH BY CATEGORY
+        </p>
         <CategoryTabs />
       </div>
-    </div>
+
+      {/* Car Carousel Section */}
+      <div className="w-[100%]  ">
+        <CarCarousel />
+      </div>
     </div>
   );
 }
