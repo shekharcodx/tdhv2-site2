b7c6ab0814dee390bb7ea2b4d8558bfc3022f7a0
Muhammad Hamza <m.hamza7265@gmail.com>
2025-10-01 22:01:13 +0500

    fix

--- src/app/catalog/[filterType]/[filterId]/loading.tsx
+++ src/app/catalog/[filterType]/[filterId]/loading.tsx
@@ -1,12 +1,28 @@
 import CatalogCardSkeleton from "@/components/catalogue/CardSkeleton";
+import catalogHeroImg from "@/assets/images/catalog-hero.svg";
+import SearchSkeleton from "@/components/catalogue/SearchSkeleton";
 
 const Loading = () => {
   return (
-    <div className="flex flex-col gap-6">
-      {Array.from({ length: 6 }).map((_, i) => (
-        <CatalogCardSkeleton key={i} />
-      ))}
-    </div>
+    <>
+      <div
+        className={`relative w-full h-[280px] md:h-[600px] mx-auto bg-cover bg-center`}
+        style={{ backgroundImage: `url(${catalogHeroImg.src})` }}
+      >
+        <div className="w-[calc(100%-32px)] md:w-[calc(100%-64px)] lg:w-[calc(100%-128px)] mx-auto flex justify-start items-center max-w-[1312px] h-full text-white">
+          <h1 className="text-base font-bold md:text-[25px] md:font-semibold text-center">
+            Catalogue
+          </h1>
+        </div>
+
+        <SearchSkeleton />
+      </div>
+      <div className="flex flex-col gap-6">
+        {Array.from({ length: 6 }).map((_, i) => (
+          <CatalogCardSkeleton key={i} />
+        ))}
+      </div>
+    </>
   );
 };
 
